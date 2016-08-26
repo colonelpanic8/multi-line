f27676da2b76be8240db1bece3ffad3c8855f811
Ivan Malison <IvanMalison@gmail.com>
2016-08-26 16:21:54 -0700

    Update commentary

--- multi-line.el
+++ multi-line.el
@@ -24,11 +24,10 @@
 ;;; Commentary:
 
 ;; multi-line aims to provide a flexible framework for automatically
-;; multi-lining and single-lining function invocations and
-;; definitions, array and map literals and more. It relies on
-;; functions that are defined on a per major mode basis wherever it
-;; can so that it behaves correctly across many different programming
-;; languages.
+;; multi-lining and single-lining function invocations and definitions,
+;; array and map literals and more. It relies on functions that are
+;; defined on a per major mode basis wherever it can so that it operates
+;; correctly across many different programming languages.
 
 ;;; Code:
 
